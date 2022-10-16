c2a65922f21b1644e1a98f3444da1290260837da
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2022-10-16 12:09:17 -0300

    create SignIn storybook with tests

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { within, userEvent, waitFor } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
+import { SignIn } from './SignIn'
+
+export default {
+  title: 'Pages/SignIn',
+  component: SignIn,
+  args: {},
+  argTypes: {},
+} as Meta
+
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    userEvent.type(
+      canvas.getByPlaceholderText('Digite seu e-mail'),
+      'matheus@gmail.com',
+    )
+    userEvent.type(canvas.getByPlaceholderText('********'), '12345678')
+    userEvent.click(canvas.getByRole('button'))
+
+    await waitFor(() => {
+      return expect(canvas.getByText('Login realizado')).ToBeInTheDocument()
+    })
+  },
+}
